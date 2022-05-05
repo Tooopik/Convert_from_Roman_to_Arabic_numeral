01cef704c4bf5d31cafaeba57cbca7f840653ae0
Adrian Wójcik <adrian.wojcik00@gmail.com>
2022-05-05 12:25:46 +0200

    First good working version

--- R_to_A_numeral_converter.py
+++ R_to_A_numeral_converter.py
@@ -8,88 +8,126 @@ numbers = {
     1000: 'M'}
 
 
-def max_number_to_use_idx(arabicNumber, numbersDict):
-    maxArabic = 0
-    for number in numbersDict:
-        if number < arabicNumber:
-            maxArabic = number
-    maxNumberToUseIdx = list(numbersDict).index(maxArabic)
-    return maxNumberToUseIdx
-
-
 def convert_number_to_rome(arabicNumber, numbersDict):
     idx = 0
     resultList = []
     equal = 0
+    maxArabic = 0
+
+    for number in numbersDict:
+        if number <= arabicNumber:
+            maxArabic = number
+    maxNumberToUseIdx = list(numbersDict).index(maxArabic)
 
     if arabicNumber in numbersDict:
         idx = list(numbersDict).index(arabicNumber)
         resultList.append(list(numbersDict)[idx])
 
     else:
-        maxNumberToUseIdx = max_number_to_use_idx(arabicNumber, numbersDict)
-        resultList.append(list(numbers)[maxNumberToUseIdx])
         while True:
-            if maxNumberToUseIdx >= 1 and maxNumberToUseIdx < 6:
-                resultList.append(list(numbersDict)[maxNumberToUseIdx - 1])
-            elif maxNumberToUseIdx == 6:
-                resultList.append(list(numbersDict)[maxNumberToUseIdx])
-            else:
-                resultList.append(list(numbersDict)[maxNumberToUseIdx])
-
-            if arabicIn == 4:
+            resultList.append(list(numbersDict)[maxNumberToUseIdx])
+            if sum(resultList) == arabicNumber and len(resultList) <= 3:
+                break
+            elif len(resultList) >= 4:
                 resultList = []
-                resultList.append(list(numbersDict)[0])
-                resultList.append(list(numbersDict)[maxNumberToUseIdx + 1])
                 break
 
-            elif arabicIn == 40:
-                resultList = []
-                resultList.append(list(numbersDict)[2])
-                resultList.append(list(numbersDict)[maxNumberToUseIdx + 1])
-                break
-
-            elif arabicIn == 400:
-                resultList = []
-                resultList.append(list(numbersDict)[4])
-                resultList.append(list(numbersDict)[maxNumberToUseIdx + 1])
-                break
-
-            else:
-                if len(resultList) <= 4:
-                    equal = sum(resultList)
-                    if equal == arabicNumber:
-                        break
-
-                else:
-                    resultList = []
+        if sum(resultList) != arabicNumber:
+            resultList.append(list(numbersDict)[maxNumberToUseIdx])
+            while True:
+                if maxNumberToUseIdx >= 1 and maxNumberToUseIdx < 6:
                     resultList.append(list(numbersDict)[maxNumberToUseIdx - 1])
+                elif maxNumberToUseIdx == 6:
+                    resultList.append(list(numbersDict)[maxNumberToUseIdx])
+                else:
+                    resultList.append(list(numbersDict)[maxNumberToUseIdx])
+
+                if arabicNumber == 4:
+                    resultList = []
+                    resultList.append(list(numbersDict)[0])
                     resultList.append(list(numbersDict)[maxNumberToUseIdx + 1])
                     break
+
+                elif arabicNumber == 40:
+                    resultList = []
+                    resultList.append(list(numbersDict)[2])
+                    resultList.append(list(numbersDict)[maxNumberToUseIdx + 1])
+                    break
+
+                elif arabicNumber == 400:
+                    resultList = []
+                    resultList.append(list(numbersDict)[4])
+                    resultList.append(list(numbersDict)[maxNumberToUseIdx + 1])
+                    break
+
+                else:
+                    if len(resultList) <= 4:
+                        equal = sum(resultList)
+                        if equal == arabicNumber:
+                            break
+
+                    else:
+                        resultList = []
+                        resultList.append(list(numbersDict)[
+                                          maxNumberToUseIdx - 1])
+                        resultList.append(list(numbersDict)[
+                                          maxNumberToUseIdx + 1])
+                        break
     return resultList
 
 
-arabicIn = 2671
+def message(final, arabicNumber):
+    result = ''
+    for i in final:
+        result += numbers[i]
+    print('*'*(26 + len(arabicNumber) + len(result)))
+    print(f'*** Arabic: {arabicNumber} | Roman: {result} ***')
+    print('*'*(26 + len(arabicNumber) + len(result)))
 
 
-arab1000 = 2000
-arab100 = 600
-arab10 = 70
-arab1 = 1
+print('Welcome to the Arabic to Roman numeral converter')
+while True:
+    print('Enter a number from 1 to 3999')
+    arabicIn = input('>>> ')
+    try:
+        if int(arabicIn) < 1 or int(arabicIn) > 3999:
+            print(f'Number {arabicIn} is wrong')
+            break
+    except ValueError:
+        print(f'The sign "{arabicIn}" is not a number!')
+        break
 
-final = []
-if arab1000 != 0:
-    final.extend(convert_number_to_rome(arab1000, numbers))
-if arab100 != 0:
-    final.extend(convert_number_to_rome(arab100, numbers))
-if arab10 != 0:
-    final.extend(convert_number_to_rome(arab10, numbers))
-if arab1 != 0:
-    final.extend(convert_number_to_rome(arab1, numbers))
+    splitNumber = []
+    for n in str(arabicIn):
+        splitNumber.append(n)
 
-result = ''
-for i in final:
-    result += numbers[i]
-print('*'*33)
-print(f'*** Liczba: {arabicIn} to: {result} ***')
-print('*'*33)
+    if len(splitNumber) >= 1:
+        arab1 = int(splitNumber[-1])
+    if len(splitNumber) >= 2:
+        arab10 = int(splitNumber[-2]) * 10
+    else:
+        arab10 = 0
+    if len(splitNumber) >= 3:
+        arab100 = int(splitNumber[-3]) * 100
+    else:
+        arab100 = 0
+    if len(splitNumber) == 4:
+        arab1000 = int(splitNumber[-4]) * 1000
+    else:
+        arab1000 = 0
+
+    final = []
+    if arab1000 != 0:
+        final.extend(convert_number_to_rome(arab1000, numbers))
+    if arab100 != 0:
+        final.extend(convert_number_to_rome(arab100, numbers))
+    if arab10 != 0:
+        final.extend(convert_number_to_rome(arab10, numbers))
+    if arab1 != 0:
+        final.extend(convert_number_to_rome(arab1, numbers))
+
+    message(final, arabicIn)
+    print('Do you want to convert another number ? (y/N)')
+    userChoice = input('>>> ')
+    if userChoice == 'N':
+        break
